ea7b65fb896f1f818b013ba5b9b6bc9acfb65aac
浅忆梦微凉 <3538922590@qq.com>
2019-01-27 21:39:23 +0800

    范围for测试通过

--- 剑指offer17_调整数组中的奇数偶数的顺序/test.cpp
+++ 剑指offer17_调整数组中的奇数偶数的顺序/test.cpp
@@ -3,25 +3,36 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//	void reOrderArray(vector<int> &array) {
+//		if (array.size() == 0 || array.size() == 1){
+//			return;
+//		}
+//		int i = 0;
+//		int j = 0;
+//		while (j < array.size()&&i < array.size()){
+//			while (i < array.size()&&array[i] % 2 != 0){
+//				++i;
+//				//找到第一个偶数
+//			}
+//			j = i + 1;
+//			while (j < array.size()&&array[j] % 2 == 0){
+//				++j;
+//				//找到第一个奇数
+//			}
+//			swap(array[i], array[j]);
+//		}
+//	}
+//};
 class Solution {
 public:
 	void reOrderArray(vector<int> &array) {
-		if (array.size() == 0 || array.size() == 1){
-			return;
-		}
-		int i = 0;
-		int j = 0;
-		while (j < array.size()&&i < array.size()){
-			while (i < array.size()&&array[i] % 2 != 0){
-				++i;
-				//找到第一个偶数
+		for (int i = 0; i < array.size(); i++){
+			for (int j = array.size() - 1; j>i; j--){
+				if (array[j] % 2 == 1 && array[j - 1] % 2 == 0)
+					swap(array[j], array[j - 1]);
 			}
-			j = i + 1;
-			while (j < array.size()&&array[j] % 2 == 0){
-				++j;
-				//找到第一个奇数
-			}
-			swap(array[i], array[j]);
 		}
 	}
 };
@@ -31,7 +42,7 @@ void TestFunc(){
 	Solution s;
 	s.reOrderArray(array);
 	for (auto i : array){
-		cout << array[i] << " ";
+		cout << i << " ";
 	}
 	cout << endl;
 }
